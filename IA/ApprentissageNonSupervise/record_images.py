b99901f3d976978fbc9ea8651ff9772ab4d2f096
Bluemax666 <44173031+Bluemax666@users.noreply.github.com>
2023-06-15 15:01:12 +0200

    Update record_images.py

--- IA/ApprentissageNonSupervise/record_images.py
+++ IA/ApprentissageNonSupervise/record_images.py
@@ -7,17 +7,23 @@ import numpy as np
 import os
 
 size = 64
+#dossier où les images seront enregistrées
+dataset_folder = "images" #***
 
 idx = 0
+#permet de capturer la webcam
 cap = cv2.VideoCapture(0)
-os.chdir("images")
+os.chdir(dataset_folder)
+#tant que le programme est actif
 while cap.isOpened():
     success, image = cap.read()
     if not success:
        pass
-   
-    image = cv2.resize(image, (256, 256))
-    cv2.imshow("image", image)
+    
+    #les images sont réduites à une taille de 64x64 pixels
+    image = cv2.resize(image, (size, size))
+    show_img = cv2.resize(image, (256, 256), cv2.INTER_AREA)
+    cv2.imshow("image", show_img)
     
     k = cv2.waitKey(10)
     if  k & 0xFF == 27 or k == ord('q'):
@@ -25,8 +31,8 @@ while cap.isOpened():
         cv2.destroyAllWindows()
         break
     
+    #si on appuye sur "r" les images sont sauvegardées
     elif k == ord('r'):
         cv2.imwrite(f"train_{idx}.png", image)
-        
         idx += 1
         
